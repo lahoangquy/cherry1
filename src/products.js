f8ccc6885e1647ad42b8094b3c1cb85701571f59
lahoangquy <62043234+lahoangquy@users.noreply.github.com>
2021-04-06 10:58:30 +0930

    Add files via upload

---
+++ src/products.js
@@ -0,0 +1,64 @@
+const products = [
+  {
+    _id: '1',
+    name: 'NT Bus Tracker ',
+    image: '/images/airpods.jpg',
+    description:
+      'A more visual interface. Real time location on screen Estimated time and the best route for your destination, Auto-mated text message service when your selected bus is nearby. A preferred route saving option',
+    target_audience: 'People of all ages',
+    rating: 4.0,
+    numReviews: 4,
+  },
+  {
+    _id: '2',
+    name: 'Money Management',
+    image: '/images/phone.jpg',
+    description:
+      'This app will track income and expenses and provide relevant information',
+    target_audience: 'working people between 18-40 years old',
+    rating: 4.0,
+    numReviews: 4,
+  },
+  {
+    _id: '3',
+    name: 'Time management app',
+    image: '/images/camera.jpg',
+    description:
+      'Helps to manage schedule as the users can see graph of time division and it helps to know the live location of the user',
+    target_audience: 'Everyone',
+    rating: 3,
+    numReviews: 3,
+  },
+  {
+    _id: '4',
+    name: 'NT Fuel and Services',
+    image: '/images/playstation.jpg',
+    description:
+      'Store fuel purchase information, analyze your fuel expenses, find local services and navigate a fuel map',
+    target_audience: 'Ages 18-40',
+    rating: 5,
+    numReviews: 3,
+  },
+  {
+    _id: '5',
+    name: 'Pocket Library Alpha',
+    image: '/images/mouse.jpg',
+    description:
+      'Pocket Library demnstrates an internal structure of CDU library (Casuarina campus) as well as major functions adn indicators',
+    target_audience: 'Students and academics',
+    rating: 3.5,
+    numReviews: 2,
+  },
+  {
+    _id: '6',
+    name: 'Simple Memo Phone App',
+    image: '/images/alexa.jpg',
+    description:
+      'A web-based memo app designede for simplicity and functionality',
+    target_audience: 'Students and academics',
+    rating: 4,
+    numReviews: 4,
+  },
+]
+
+export default products
